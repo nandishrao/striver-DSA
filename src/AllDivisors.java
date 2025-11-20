4dc2202120f94fe4a8a0bafeb8722304142587f9
Nandish Rao <nandishhraoo@gmail.com>
2025-11-20 19:33:29 +0530

    All Divisors problem

---
+++ src/AllDivisors.java
@@ -0,0 +1,10 @@
+public class AllDivisors {
+    public static void main(String[] args) {
+        int n = 32;
+        for (int i = 1 ; i<=n ; i++) {
+            if(n%i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
