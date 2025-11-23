ba6d59378a39a8185154ea0723bf4c0cf0c6917d
Nandish Rao <nandishhraoo@gmail.com>
2025-11-23 22:23:27 +0530

    To check if a string is a palindrome using Two pointers

---
+++ src/StringPalindrome.java
@@ -0,0 +1,25 @@
+class PalinString{
+    public static boolean ValidateStringPalindrome(String s){
+        int left = 0;
+        int right = s.length() -1;
+
+       while(left < right){
+           if(s.charAt(left) != s.charAt(right)){
+               return false;
+           }else{
+               left++;
+               right--;
+           }
+       }
+        return true;
+
+    }
+}
+
+public class StringPalindrome {
+    public static void main(String[] args) {
+        PalinString p = new PalinString();
+        boolean result =p.ValidateStringPalindrome("A man, a plan, a canal: Panama");
+        System.out.println(result);
+    }
+}
