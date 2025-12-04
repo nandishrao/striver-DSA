5a1b7957ed03759ac0ede39e6e5f3d19174a3da1
Nandish Rao <nandishhraoo@gmail.com>
2025-12-04 12:04:55 +0530

    largest number in an array using Java

---
+++ src/LargestElement.java
@@ -0,0 +1,14 @@
+
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int [] arr ={10,20,30,40,50,0};
+        int largest = 0;
+        for (int i =0 ; i<arr.length ; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        System.out.print(""+largest);
+    }
+}
