14da0c55d05c98814a1c687df5e1cdda59a75083
Nandish Rao <nandishhraoo@gmail.com>
2026-01-01 20:14:07 +0530

    Find the number that appears once, and other numbers twice in java

---
+++ src/SingleNumber.java
@@ -0,0 +1,19 @@
+public class SingleNumber {
+    public static void main(String[] args) {
+        Solution_SingleNum sol = new Solution_SingleNum();
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(sol.singleNumber(nums));
+    }
+}
+
+class Solution_SingleNum {
+    public int singleNumber(int[] arr) {
+        int result = 0;
+
+        for (int num : arr) {
+            result = result ^ num;
+        }
+
+        return result;
+    }
+}
