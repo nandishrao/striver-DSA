70a47e911de96d247130c365dd8a320c5661f836
Nandish Rao <nandishhraoo@gmail.com>
2026-01-09 12:48:48 +0530

    counting frequency in java

--- src/CountingFrequency.java
+++ src/CountingFrequency.java
@@ -4,12 +4,13 @@ public class CountingFrequency {
     public static void main(String[] args) {
         int [] arr = {1,1,1,3,4,2,1,3,10,10,4,12,13,10};
         HashMap <Integer , Integer> h =new HashMap<Integer,Integer>();
+         int MaxElement = 0;
         for (int i =0 ; i < arr.length ; i++){
             if(h.containsKey(arr[i])){
                 h.put(arr[i], h.get(arr[i]) + 1);
 
-            }else{
-                h.put(arr[i] , 1);
+            }else {
+                h.put(arr[i], 1);
             }
         }
         System.out.println(h);
